5219bfe390eb06b48494ca48ad9ef6b5816e60dc
aballinger1981 <andrew@andrewballinger.com>
2017-07-07 15:57:19 -0400

    set animation on game board and player selection divs

---
+++ script.js
@@ -0,0 +1,40 @@
+// this is required for the (not so) edge case where your script is loaded after the document has loaded
+// https://developer.mozilla.org/en/docs/Web/API/Document/readyState
+if (document.readyState !== 'loading') {
+  ready();
+} else {
+  // the document hasn't finished loading/parsing yet so let's add an event handler
+  document.addEventListener('DOMContentLoaded', ready);
+}
+
+let numberOfPlayers;
+let xOrO;
+
+function ready() {
+  playerSelectHandler();
+}
+
+function playerSelectHandler() {
+  const playerChoice = document.getElementsByClassName("player-choice");
+
+  for (let i = 0; i < playerChoice.length; i++) {
+    playerChoice[i].addEventListener("click", function () {
+      const playerSelect = document.getElementById("player-select");
+      const gameBoard = document.getElementById("game-board");
+      const playerData = document.getElementById("player-data");
+      const reset = document.getElementById("reset-container");
+
+      playerSelect.setAttribute("style", "animation: fadeOut .5s linear forwards");
+      gameBoard.setAttribute("style", "animation: fadeIn 1s linear forwards");
+      playerData.setAttribute("style", "animation: fadeIn 1s linear forwards");
+      reset.setAttribute("style", "animation: fadeIn 1s linear forwards");
+
+      numberOfPlayers = playerChoice[i].id;
+    });
+  }
+  gamePieceSelectionHandler();
+}
+
+function gamePieceSelectionHandler() {
+
+}
